99c30354de0fd3af787dd2adc50bedd21f1d28b3
inlancersystem4 <inlancersystem4@gmail.com>
2024-02-01 15:21:49 +0530

    add files and code

---
+++ js/root.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            screens: {
+                'mobileUp': '460px',
+            },
+            colors: {
+                'themeColor1': '#FEC3A6',
+                'themeColor2': '#685044',
+                'themeColor3': '#DDDBCC',
+                'themeColor4': '#D1DDCC',
+                'themeColor5': '#DDD1CC',
+                'themeColor6': '#000000',
+            },
+        }
+    }
+}
